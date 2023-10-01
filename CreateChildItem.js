2dc37bf767e827ea0e59453b65d1d46d0e015e38
olgaml <90445794+olgaml@users.noreply.github.com>
2023-10-01 09:08:16 +0300

    Update CreateChildItem.js

--- CreateChildItem.js
+++ CreateChildItem.js
@@ -1,9 +1,10 @@
 const exampleBtn = document.getElementById('createGlobalItem');
-
+alert('start')
 exampleBtn.addEventListener('click', () => {
   //window.open('https://olgame.atlassian.net/jira/secure/CreateIssue.jspa', '_blank');
 //window.open('https://olgame.atlassian.net/jira/secure/CreateIssue.jspa',"Ratting","width=550,height=170,left=150,top=200,toolbar=0,status=0,");
  window.open('https://olgame.atlassian.net/jira/secure/CreateIssue.jspa','New Issue','width=600,height=600,location=no,toolbar=no,menubar=no' )
   var menubar= document.getElementById('ak-jira-navigation')
   menubar.style.display = 'none';
+  alert('end')
 });
