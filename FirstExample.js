a7189c6c656e705fcc65cfb6924fb2bd54c9b010
olgaml <90445794+olgaml@users.noreply.github.com>
2024-01-13 19:32:46 +0200

    Update FirstExample.js

--- FirstExample.js
+++ FirstExample.js
@@ -1,15 +1,5 @@
 function showDetails() {
 
-  const Bridge = window.AdaptavistBridgeContext;
-  
-  AdaptavistBridge.request({
-    url: `/rest/api/2/issue/${AdaptavistBridgeContext.context.issueKey}`,
-    type: 'GET'
-})
-    .then(issue => {
-        console.log('issue', issue)
-        document.getElementById("issueType").value = `${issue.key} is a ${issue.fields.issuetype.name} in ${issue.fields.status.name}`;
-    });
 
-
+document.getElementById("issueType").value = "My TEST"
 }
